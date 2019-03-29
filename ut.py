87726b2e58ab3c930dc6bd1828ed572256b1dc6f
Li Nanxian <SENSETIME\linanxian1@g114e1800327.domain.sensetime.com>
2019-03-29 14:32:23 +0800

    s

--- ut.py
+++ ut.py
@@ -4,7 +4,7 @@ from app import *
 
 class Add_Test(unittest.TestCase):
     def test_adding(self):
-        self.assertEqual(adding(1,1), 2)
+        self.assertEqual(adding(1,1), 3)
 
 
 if __name__ == "__main__":
